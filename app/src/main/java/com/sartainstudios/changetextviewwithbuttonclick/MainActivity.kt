361d239430872693dd686f20ed990d02c4f1276b
jhnsartain3 <=>
2019-04-14 17:41:37 -0500

    Initial commit

---
+++ app/src/main/java/com/sartainstudios/changetextviewwithbuttonclick/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.sartainstudios.changetextviewwithbuttonclick
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonChangeDisplayText.setOnClickListener {
+            textViewDisplayTextChange.text = "Text view text has changed \nThanks for watching John's Android Studio Tutorials"
+        }
+    }
+}
